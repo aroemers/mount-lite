1f4617b26261070e825af265b423bb50e7696706
Arnout Roemers <github@functionalbytes.nl>
2016-02-03 14:01:07 +0100

    Clean up use of ExecutorService

--- src/mount/parallel.clj
+++ src/mount/parallel.clj
@@ -1,6 +1,7 @@
 (ns mount.parallel
   "Namepace responsible for starting and stopping a state var
   dependency graph using a Thread pool."
+  {:no-doc true}
   (:require [clojure.set :as set]
             [com.stuartsierra.dependency :as dep]
             [mount.graph :as graph])
@@ -9,37 +10,56 @@
 (defn- fixed-pool [t]
   (Executors/newFixedThreadPool t))
 
-(defn- action [vars action-f next-f deps-f threads]
-  (let [graph       (graph/var-graph vars)
-        pool        (fixed-pool threads)
-        thrown      (promise)
-        todonext    (atom {:todo (set (dep/nodes graph)) :done () :next nil})
-        mk-task-f   #(try (%) (catch Throwable e (deliver thrown e) (.shutdown pool)))
-        next-task-f (fn next-task-f [var]
-                      (let [todonext' (swap! todonext
-                                             (fn [{:keys [todo done]}]
-                                               (let [todo' (disj todo var)
-                                                     done' (cons var done)]
-                                                 {:todo todo' :done done'
-                                                  :next (remove #(some todo' (deps-f graph %))
-                                                                (next-f graph var))})))]
-                        (if (and (empty? (:todo todonext')) (empty? (:next todonext')))
-                          (.shutdown pool)
-                          (doseq [next (:next todonext')]
-                            (.submit pool (mk-task-f #(do (action-f next) (next-task-f next))))))))]
-    (doseq [var (filter #(empty? (deps-f graph %)) (dep/nodes graph))]
-      (.submit pool (mk-task-f #(do(action-f var) (next-task-f var)))))
-    (.awaitTermination pool 24 TimeUnit/HOURS) ;;---TODO Make configurable?
-    (when (realized? thrown)
-      (throw @thrown))
-    (reverse (:done @todonext))))
+(defn- work-task [pool thrown task-f done-f task]
+  (fn []
+    (try
+      (task-f task)
+      (let [[done? forks] (done-f task)]
+        (if-not done?
+          (doseq [fork forks]
+            (.submit pool (work-task pool thrown done-f task-f fork)))
+          (.shutdown pool)))
+      (catch Throwable t
+        (.shutdown pool)
+        (deliver thrown t)))))
 
-(defn start [vars start-f threads]
+(defn- work [pool task-f done-f & initial]
+  (let [thrown (promise)]
+    (doseq [init initial]
+      (.submit pool (work-task pool thrown task-f done-f init)))
+    (.awaitTermination pool 24 TimeUnit/HOURS)
+    (when (realized? thrown)
+      (throw @thrown))))
+
+(defn- action [vars action-f next-f deps-f threads]
+  (let [graph    (graph/var-graph vars)
+        pool     (fixed-pool threads)
+        acc      (atom {:todo (set (dep/nodes graph)) :done () :next nil})
+        done-f   (fn [var]
+                   (let [acc' (swap! acc
+                                     (fn [{:keys [todo done]}]
+                                       (let [todo' (disj todo var)
+                                             done' (cons var done)]
+                                         {:todo todo' :done done'
+                                          :next (remove #(some todo' (deps-f graph %))
+                                                        (next-f graph var))})))]
+                     [(empty? (:todo acc')) (:next acc')]))
+        initials (filter #(empty? (deps-f graph %)) (dep/nodes graph))]
+    (apply work (fixed-pool threads) action-f done-f initials)
+    (reverse (:done @acc))))
+
+(defn start
+  "Start the given vars, in parallel where applicable, using the given
+  start-f function and a maximum number of threads."
+  [vars start-f threads]
   (let [next-f dep/immediate-dependents
         deps-f dep/immediate-dependencies]
     (action vars start-f next-f deps-f threads)))
 
-(defn stop [vars stop-f threads]
+(defn stop
+  "Stop the given vars, in parallel where applicable, using the given
+  start-f function and a maximum number of threads."
+  [vars stop-f threads]
   (let [next-f dep/immediate-dependencies
         deps-f dep/immediate-dependents]
     (action vars stop-f next-f deps-f threads)))
