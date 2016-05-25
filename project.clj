7f4e075216a2c16755e521d9951f4b3927a0fc7a
Arnout Roemers <github@functionalbytes.nl>
2016-05-25 22:39:28 +0200

    Version 0.9.8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject functionalbytes/mount-lite "0.9.8-SNAPSHOT"
+(defproject functionalbytes/mount-lite "0.9.8"
   :description "Mount, but Clojure only and a more flexible API."
   :url "https://github.com/aroemers/mount-lite"
   :license {:name "Eclipse Public License"
