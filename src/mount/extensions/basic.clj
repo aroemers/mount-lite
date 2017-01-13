e4d9cb4aa051434db5af84ce3a8b973a2e72151a
Arnout Roemers <github@functionalbytes.nl>
2017-01-13 11:23:27 +0100

    Fix basic extensions macros using private fns

--- src/mount/extensions/basic.clj
+++ src/mount/extensions/basic.clj
@@ -1,7 +1,8 @@
 (ns mount.extensions.basic
   "Basic extensions for influencing which states are started or
   stopped."
-  (:require [mount.lite :as mount]))
+  (:require [mount.lite :as mount]
+            [mount.utils :as utils]))
 
 (defn- with-states
   [state-kws f]
@@ -13,12 +14,12 @@
   state vars, inside the body. The state vars don't need to be in
   dependency order, this is deduced from the root `*states*` binding."
   [state-vars & body]
-  `(with-states (filter (set (map var->keyword ~state-vars)) @mount/*states*)
+  `(#'with-states (filter (set (map utils/var->keyword ~state-vars)) @mount/*states*)
      (fn [] ~@body)))
 
 (defmacro with-except
   "Reduce the available states for starting or stopping, to all the
   current `*states*` except for the given state vars, inside the body."
   [state-vars & body]
-  `(with-states (remove (set (map var->keyword ~state-vars)) @mount/*states*)
+  `(#'with-states (remove (set (map utils/var->keyword ~state-vars)) @mount/*states*)
      (fn [] ~@body)))
