23672a0a990c5b8aec5c06731fa16790c509f842
Arnout Roemers <github@functionalbytes.nl>
2017-08-24 15:52:23 +0200

    Release 2.0.0
    It has been stable for a while now.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject functionalbytes/mount-lite "2.0.0-SNAPSHOT"
+(defproject functionalbytes/mount-lite "2.0.0"
   :description "mount, but different and light"
   :url "https://github.com/aroemers/mount-lite"
   :license {:name "Eclipse Public License"
