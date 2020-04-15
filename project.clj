50e0c0cc9d47a8712dfe43948be4b154b4c7b257
Arnout Roemers <github@functionalbytes.nl>
2020-04-15 10:19:33 +0200

    Prepare snapshot version

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
-(defproject functionalbytes/mount-lite "2.1.3"
+(defproject functionalbytes/mount-lite "3.0.0-SNAPSHOT"
   :description "mount, but different and light"
   :url "https://github.com/aroemers/mount-lite"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :profiles {:test {:dependencies [[org.clojure/tools.namespace "0.2.11"]]}})
+  :dependencies [[org.clojure/clojure "1.8.0"]])
