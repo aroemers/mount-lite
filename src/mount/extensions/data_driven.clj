3ad514c63ca4b73f60bb802b087cb00a4b7ec60e
Arnout Roemers <roemers@zorgdomein.nl>
2020-04-18 21:06:49 +0200

    Expand data-driven extension

--- src/mount/extensions/data_driven.clj
+++ src/mount/extensions/data_driven.clj
@@ -19,15 +19,27 @@
     (mount/with-substitutes substitutes
       (f))))
 
+(defn- wrap-with-system-map [f system-map]
+  (fn []
+    (mount/with-system-map system-map
+      (f))))
+
+(defn- wrap-with-system-key [f system-key]
+  (fn []
+    (mount/with-system-key system-key
+      (f))))
+
 (defn- resolve* [symbol]
   (-> symbol resolve deref))
 
 (defn with-config*
-  [{:keys [only except substitutes]} f]
+  [{:keys [only except substitutes system-map system-key]} f]
   ((cond-> f
-     only        (wrap-with-only (map resolve* only))
-     except      (wrap-with-except (map resolve* except))
-     substitutes (wrap-with-substitutes (reduce-kv (fn [a k v] (assoc a (resolve* k) (resolve* v))) {} substitutes)))))
+     only        (wrap-with-only        (map resolve* only))
+     except      (wrap-with-except      (map resolve* except))
+     substitutes (wrap-with-substitutes (reduce-kv (fn [a k v] (assoc a (resolve* k) (resolve* v))) {} substitutes))
+     system-map  (wrap-with-system-map  (reduce-kv (fn [a k v] (assoc a (resolve* k) v))            {} system-map))
+     system-key  (wrap-with-system-key  system-key))))
 
 (defmacro with-config
   [config & body]
