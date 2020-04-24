0f1b4f7dc76e12908aa9e83be14c9b378a1aead4
Arnout Roemers <roemers@zorgdomein.nl>
2020-04-24 09:18:59 +0200

    Move tools.namespace dependency back to profiles

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "https://github.com/aroemers/mount-lite"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.namespace "0.2.11"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]]}
+             :test {:dependencies [[org.clojure/tools.namespace "0.2.11"]]}})
