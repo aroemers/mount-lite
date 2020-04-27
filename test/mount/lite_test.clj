6128262b42a291c26db4366bf1c9534ec4f0a4da
Arnout Roemers <roemers@zorgdomein.nl>
2020-04-27 20:41:32 +0200

    More unit tests

--- test/mount/lite_test.clj
+++ test/mount/lite_test.clj
@@ -1,24 +1,65 @@
 (ns mount.lite-test
   (:require [clojure.test :refer [deftest testing is]]
             [mount.lite :as sut]
-            [mount.protocols :as protocols]))
+            [mount.protocols :as protocols]
+            mount.report))
 
 (deftest test-state
-  (testing "state should create an IState implementation"
+  (testing "Macro `state` should create an IState implementation"
     (let [state (sut/state :start 1 :stop this)]
       (is (satisfies? protocols/IState state))
 
-      (testing "which should start"
-        (is (= (protocols/start state) 1)))
+      (testing "which can start"
+        (is (= 1 (protocols/start state))))
 
-      (testing "which should stop, with access to this"
-        (is (= (protocols/stop state) 1))))))
+      (testing "which can stop, using implicit this"
+        (is (= 1 (protocols/stop state)))))))
+
 
 (deftest test-defstate
-  (testing "defstate should define a global state"
-    (let [var (sut/defstate foo :start 1 :stop @foo)]
-      (is (var? var))
+  (testing "Macro `defstate` should define a global state"
 
-      (testing "which should start"
-        (let [started (sut/start)]
-          (is (contains? (set started) foo)))))))
+    (testing "without any expression"
+      (is (var? (sut/defstate foo))))
+
+    (testing "with a start expression"
+      (is (var? (sut/defstate foo :start 1))))
+
+    (testing "with a stop expression refering to itself"
+      (is (var? (sut/defstate foo :start 1 :stop @foo))))
+
+    (testing "with redefinitions considered equal"
+      (sut/defstate foo :start 1 :stop 2)
+      (let [current foo]
+        (sut/defstate foo :start 2 :stop 3)
+        (is (= current foo))))
+
+    (testing "which can be dereferenced once started"
+      (sut/defstate foo :start 1)
+      (protocols/start foo)
+      (try
+        (is (= 1 @foo))
+        (finally
+          (protocols/stop foo))))
+
+    (testing "without overriding started stop logic"
+      (let [val (atom :untouched)]
+        (sut/defstate foo :stop (reset! val :correct))
+        (protocols/start foo)
+        (sut/defstate foo :stop (reset! val :wrong))
+        (protocols/stop foo)
+        (is (= :correct @val))))))
+
+
+(deftest test-start
+
+  (sut/defstate foo :start 1 :stop (reset! val :correct))
+  (protocols/stop foo)
+
+  (testing "Calling `start`"
+
+    (testing "should return the started states"
+      (is (= (list foo) (sut/start))))
+
+    (testing "should have started the states"
+      (is (= 1 @foo)))))
