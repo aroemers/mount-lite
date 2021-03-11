6820d54bedf592836ccad59a40eac3282de8e88d
Darin Douglass <ddouglass@barracuda.com>
2021-03-11 14:51:13 -0500

    add tests

---
+++ test/mount/extensions/autostart_test.clj
@@ -0,0 +1,34 @@
+(ns mount.extensions.autostart-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [mount.extensions.autostart :as sut]
+            [mount.extensions.explicit-deps :as deps]
+            [mount.lite :as mount]))
+
+(sut/defstate first
+  :dependencies []
+  :start 42)
+
+(sut/defstate second
+  :dependencies []
+  :start 420)
+
+(sut/defstate third
+  :dependencies [#'first #'second]
+  :start (+ @first @second))
+
+(defn fully?
+  [state]
+  (= {#'first state #'second state #'third state} (mount/status)))
+
+(deftest autostart-test
+  (testing "with default autostart-fn"
+    (= 462 @third)
+    (is (fully? :started))
+    (mount/stop)
+    (is (fully? :stopped)))
+  (testing "with custom autostart-fn"
+    (sut/set-autostart-fn! deps/start)
+    (= 462 @third)
+    (is (fully? :started))
+    (mount/stop)
+    (is (fully? :stopped))))
