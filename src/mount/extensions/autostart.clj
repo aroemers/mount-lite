8ac09790837ff7b608cc3848ae3f0cc39a19022b
Darin Douglass <ddouglass@barracuda.com>
2021-03-11 14:41:44 -0500

    Add autostart extension
    - Unlike other extensions, this one provides a new `defstate` macro that
      wraps `mount.lite/defstate`s in auto-start functionality.
    - Synchronize startups across threads just in case derefs happen in
      multiple threads.
    - `set-autostart-fn!` allows specifying of different start
      functionalities (i.e. standard `mount.lite/start` vs extension starts)

---
+++ src/mount/extensions/autostart.clj
@@ -0,0 +1,49 @@
+(ns mount.extensions.autostart
+  (:require [mount.lite :as mount])
+  (:import [clojure.lang IDeref]
+           [mount.lite IState]))
+
+(defonce
+  ^{:doc "The fn that will be called to automatically start any state
+    that has been deref'd but not started. If this fn is unsuccessful
+    in starting the state, the standard error handling will occur."}
+  autostart-fn mount/start)
+
+(defn set-autostart-fn!
+  "Configures `autostart-fn` use by `AutoStartState` during state deref."
+  [fn]
+  (alter-var-root #'autostart-fn (constantly fn)))
+
+(defrecord AutoStartState [var state]
+  IState
+  (start* [_]
+    (mount/start* state))
+
+  (stop* [_]
+    (mount/stop* state))
+
+  (status* [_]
+    (mount/status* state))
+
+  (properties [_]
+    (mount/properties state))
+
+  IDeref
+  (deref [_]
+    (if (= :started (mount/status* state))
+      @state
+      ;; synchronize access across thread boundaries just in case
+      ;; two threads want to access/start this state at the same time.
+      (locking var
+        (autostart-fn var)
+        @state))))
+
+(defmacro defstate
+  "Defines a state that should be auto-started on first deref.
+
+  See `mount.lite/defstate` for more information."
+  [name & args]
+  `(do
+     (mount/defstate ~name ~@args)
+     (alter-var-root (var ~name) (fn [state#] (->AutoStartState (var ~name) state#)))
+     (var ~name)))
