b5e7c6d5027d24ca1449f313dec74cee4466a2b1
Arnout Roemers <roemers@zorgdomein.nl>
2021-03-20 11:47:48 +0100

    Fix autostart extension tests

--- test/mount/extensions/autostart_test.clj
+++ test/mount/extensions/autostart_test.clj
@@ -1,38 +1,55 @@
 (ns mount.extensions.autostart-test
-  (:require [clojure.test :refer [deftest is testing]]
+  (:require [clojure.test :refer [deftest is testing use-fixtures]]
             [mount.extensions.autostart :as sut]
             [mount.extensions.explicit-deps :as deps]
             [mount.lite :as mount]))
 
-(sut/defstate first
-  :dependencies []
-  :start 42)
+;;; Define extra states and clean them up afterwards.
 
-(sut/defstate second
-  :dependencies []
-  :start 420)
+(use-fixtures :each
+  (fn [f]
+    (mount/stop)
 
-(sut/defstate third
-  :dependencies [#'first]
-  :start (* 2 @first))
+    (let [before-states @mount/*states*]
 
-(defn in-state?
-  [states]
-  (= (zipmap [#'first #'second #'third] states) (mount/status)))
+      (sut/defstate state-a
+        :dependencies []
+        :start 42)
+
+      (sut/defstate state-b
+        :dependencies []
+        :start 420)
+
+      (sut/defstate state-c
+        :dependencies [#'state-a]
+        :start (* 2 @state-a))
+
+      (try
+        (f)
+        (finally
+          (mount/stop)
+          (reset! mount/*states* before-states))))))
+
+
+;;; Actual tests
+
+(defn is-status
+  [statuses]
+  (is (= statuses (select-keys (mount/status) (keys statuses)))))
 
 (deftest autostart-test
   (testing "with default autostart-fn"
-    (= 84 @third)
-    (is (in-state? [:started :started :started]))
+    (= 84 @state-c)
+    (is-status {#'state-a :started #'state-b :started #'state-c :started})
     (mount/stop)
-    (is (in-state? [:stopped :stopped :stopped])))
-  (testing "with custom autostart-fn"
+    (is-status {#'state-a :stopped #'state-b :stopped #'state-c :stopped}))
+
+  (testing "with custom autostart-fn (explicit-deps)"
     (try
       (sut/set-autostart-fn! deps/start)
-      (= 84 @third)
-      (testing "explicit deps still only starts states in its deps tree"
-        (is (in-state? [:started :stopped :started])))
+      (= 84 @state-c)
+      (is-status {#'state-a :started #'state-b :stopped #'state-c :started})
       (mount/stop)
-      (is (in-state? [:stopped :stopped :stopped]))
+      (is-status {#'state-a :stopped #'state-b :stopped #'state-c :stopped})
       (finally
         (sut/set-autostart-fn! mount/start)))))
